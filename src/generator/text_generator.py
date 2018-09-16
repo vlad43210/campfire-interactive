5e463139e2606914a78201573cae5aeecbb30fed
Vladimir Barash <vlad.barash@graphika.com>
2018-09-16 09:30:42 -0400

    loading test file, generating character hash

--- src/generator/text_generator.py
+++ src/generator/text_generator.py
@@ -1,3 +1,4 @@
+import os, sys
 import numpy as np
 import pandas as pd
 from keras.models import Sequential
@@ -5,3 +6,11 @@ from keras.layers import Dense
 from keras.layers import Dropout
 from keras.layers import LSTM
 from keras.utils import np_utils
+
+root_folder = os.path.dirname(os.path.dirname(os.path.dirname(os.path.realpath(__file__))))
+text=(open("{0}/test/pg1041.txt".format(root_folder,)).read())
+text=text.lower()
+
+characters = sorted(list(set(text)))
+n_to_char = {n:char for n, char in enumerate(characters)}
+char_to_n = {char:n for n, char in enumerate(characters)}
